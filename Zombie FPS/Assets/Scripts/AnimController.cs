392ce297bbb3833172e8113efc2e7fb715fda234
Overcrewt <dhjs58@gmail.com>
2018-05-06 23:30:36 +0300

    Added some scripts

--- Zombie FPS/Assets/Scripts/AnimController.cs
+++ Zombie FPS/Assets/Scripts/AnimController.cs
@@ -3,14 +3,24 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class AnimController : MonoBehaviour {
-    [SerializeField] Animator animator;
+    
+    Animator playerAnim;
 	// Use this for initialization
-	void Start () {
-        animator = GetComponent<Animator>();
+	void Start ()
+    {
+       
+        playerAnim = this.GetComponent<Animator>();
+    
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		
-	}
+	void Update ()
+    {
+        playerAnim.SetFloat("inputX", GameManager.Instance.InputController.Vertical);
+        playerAnim.SetFloat("inputY", GameManager.Instance.InputController.Horizontal);
+
+        playerAnim.SetBool("crouch", GameManager.Instance.InputController.isCrouched);
+        playerAnim.SetBool("walk", GameManager.Instance.InputController.isWalking);
+        playerAnim.SetBool("isSprinting", GameManager.Instance.InputController.isSprinting);
+    }
 }
