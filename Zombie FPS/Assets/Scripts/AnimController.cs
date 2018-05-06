0747c3a043ac17d669735686c36b985a7e804528
Overcrewt <dhjs58@gmail.com>
2018-05-06 21:38:30 +0300

    Walk Animation Developing

---
+++ Zombie FPS/Assets/Scripts/AnimController.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimController : MonoBehaviour {
+    [SerializeField] Animator animator;
+	// Use this for initialization
+	void Start () {
+        animator = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
